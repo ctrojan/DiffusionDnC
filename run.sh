599a61c7eecbccb64ecfcfeaa001f64bc889d233
ctrojan <98951993+ctrojan@users.noreply.github.com>
2024-05-14 14:52:04 +0100

    Added higher dimensional logistic regression

--- run.sh
+++ run.sh
@@ -35,3 +35,12 @@ if [[ "$1" == "spambase" ]] || [[ "$1" == "all" ]]; then
 	Rscript ./experiments/spambase/spambase_semikde.R
 	python3 ./experiments/spambase/spambase_comparison.py	
 fi
+
+if [[ "$1" == "bigtoylogreg" ]] || [[ "$1" == "all" ]]; then
+	echo "Higher dimensional logistic regression"
+	python3 ./experiments/toy_logistic_regression_bigger/bigtoylogreg_setup.py
+	python3 ./experiments/toy_logistic_regression_bigger/bigtoylogreg_diffusion.py
+	python3 ./experiments/toy_logistic_regression_bigger/bigtoylogreg_gp.py
+	Rscript ./experiments/toy_logistic_regression_bigger/bigtoylogreg_semikde.R
+	python3 ./experiments/toy_logistic_regression_bigger/bigtoylogreg_comparison.py	
+fi
